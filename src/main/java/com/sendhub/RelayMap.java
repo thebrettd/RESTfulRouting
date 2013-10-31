75cdf16a2f5069d987857fb8ff463bf15fdd631c
thebrettd <thebrettd@gmail.com>
2013-10-30 23:09:45 -0700

    Add some comments. Only create relayMap when 1 or more USPhoneNumbers are passed in

--- src/main/java/com/sendhub/RelayMap.java
+++ src/main/java/com/sendhub/RelayMap.java
@@ -7,6 +7,14 @@ public class RelayMap {
 
     private Map<String,List<USPhoneNumber>> relayMapping;
 
+    /***
+     * This will create a HashMap whose key is a URL and whose value is a list of USPhoneNumbers that will be sent to
+     * that relay.
+     *
+     * Assumes that all USPhoneNumbers passed in are valid.
+     *
+     * @param USPhoneNumbers
+     */
     public RelayMap(List<USPhoneNumber> USPhoneNumbers) {
         this.relayMapping = new HashMap<String, List<USPhoneNumber>>(0);
         mapToRelays(USPhoneNumbers);
@@ -17,19 +25,18 @@ public class RelayMap {
      * @param USPhoneNumbers
      */
     private void mapToRelays(List<USPhoneNumber> USPhoneNumbers) {
-
-        List<USPhoneNumber> remainingUSPhoneNumbers = mapToRelay(USPhoneNumbers, 25, "10.0.4.0/24");
-        remainingUSPhoneNumbers = mapToRelay(remainingUSPhoneNumbers, 10, "10.0.3.0/24");
-        remainingUSPhoneNumbers = mapToRelay(remainingUSPhoneNumbers, 5, "10.0.2.0/24");
-        remainingUSPhoneNumbers = mapToRelay(remainingUSPhoneNumbers, 1, "10.0.1.0/24");
+        if(USPhoneNumbers.size() > 0){
+            List<USPhoneNumber> remainingUSPhoneNumbers = mapToRelay(USPhoneNumbers, 25, "10.0.4.0/24");
+            remainingUSPhoneNumbers = mapToRelay(remainingUSPhoneNumbers, 10, "10.0.3.0/24");
+            remainingUSPhoneNumbers = mapToRelay(remainingUSPhoneNumbers, 5, "10.0.2.0/24");
+            mapToRelay(remainingUSPhoneNumbers, 1, "10.0.1.0/24");
+        }
     }
 
     /***
      * Attempts to map phone numbers to a relay.
      *
      * If there are not enough phone numbers to fully utilize this relay, it will not use this relay.
-     *
-     *
      *
      * @param USPhoneNumbers The set of phone numbers with you are trying to map to the current relay
      * @param relayThroughput The throughput of the current relay
